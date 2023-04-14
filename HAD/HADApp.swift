60015ad1b77db425fe19a3c567d72621070a90f6
imacbig03 <charlie.cai@g.feitianacademy.org>
2023-04-14 15:50:35 -0400

    Initial Commit

---
+++ HAD/HADApp.swift
@@ -0,0 +1,17 @@
+//
+//  HADApp.swift
+//  HAD
+//
+//  Created by iMacBig03 on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct HADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
